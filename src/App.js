f4d9430bb68fff6099a867b1f2e7e67834d6f72f
jasmeet singh <asujasu71@gmail.com>
2025-07-23 16:26:25 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import React, {useState} from "react";
 import ReactDOM from "react-dom/client";
 import FoodOption from "./component/FoodOption";
 import Header from "./component/Header";
+import Home from "./component/Home";
 import GroceryOption from "./component/GroceryOption";
 import DineOption from "./component/DineOption";
 import Restaurant from "./component/Restaurant";
-
+import { BrowserRouter, Routes, Route } from "react-router";
+import RestaurantMenu from "./Component/RestaurantMenu";
 
 function App()
 {
     return(
    <>
-   <Header />
-   <FoodOption/>
-   <GroceryOption/>
-   <DineOption/>
-   <Restaurant/>    
+    <BrowserRouter>
+       <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/restaurant" element={<Restaurant></Restaurant>}></Route>
+        <Route path="/city/delhi/:id" element={<RestaurantMenu></RestaurantMenu>}></Route>
+       </Routes>
+       </BrowserRouter>   
    </>
     )
 }
--- src/component/DineCard.js
+++ src/component/DineCard.js
@@ -1,3 +1,5 @@
+
+
 export default function DineCard({RestData}){
 
 
