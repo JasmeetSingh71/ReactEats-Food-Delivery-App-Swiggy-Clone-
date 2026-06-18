26e166b0af8a76085b8f07e4cb5cfc0a5e234eb6
jasmeet singh <asujasu71@gmail.com>
2026-06-18 14:20:45 +0530

    api changes

--- src/component/Restaurant.js
+++ src/component/Restaurant.js
@@ -10,8 +10,8 @@ export default function Restaurant(){
     
      async function fetchData() {
         
-        const proxyServer = "https://thingproxy.freeboard.io/fetch/"
-        const swiggyAPI = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true";
+        const proxyServer = "https://cors-anywhere.herokuapp.com/"
+        const swiggyAPI = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=32.73630&lng=74.86510&is-seo-homepage-enabled=true";
         const response = await fetch(proxyServer+swiggyAPI);
         const data = await response.json();
         setRestData(data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
@@ -19,6 +19,16 @@ export default function Restaurant(){
 
      fetchData();
     },[])
+
+
+
+
+
+
+
+
+
+
 
     // console.log(RestData);
      if(RestData.length==0)
--- src/component/RestaurantMenu.js
+++ src/component/RestaurantMenu.js
@@ -14,7 +14,7 @@ export default function RestaurantMenu(){
     
         async function fetchData() {
            
-           const proxyServer = "https://thingproxy.freeboard.io/fetch/"
+           const proxyServer = "https://cors-anywhere.herokuapp.com/"
            const swiggyAPI = `https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=${id}`;
            const response = await fetch(proxyServer+swiggyAPI);
            const data = await response.json();
